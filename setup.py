41901790f50610580238d5ab3b378362e6734344
Jyotirmoy Bhattacharya <jyotirmoy@jyotirmoy.net>
2012-08-15 09:30:52 +0530

    Original commit.
    Got the source code from Darren Wilkinson's site
    http://www.staff.ncl.ac.uk/d.j.wilkinson/software/isbn.py
    
    Packaged with distutils.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='isbn',
+      version='1.0',
+      description='ISBN validation and conversion utilities',
+      author='Darren J Wilkinson',
+      maintainer='Jyotirmoy Bhattacharya',
+      maintainer_email='jyotirmoy@jyotirmoy.net',
+      license = 'GPL',
+      py_modules=['isbn'],
+     )
